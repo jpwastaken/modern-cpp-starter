b430a7c62180769ea13a6b30ecc3a979f25fc3bf
JpDev <jpwastaken@users.noreply.github.com>
2026-02-03 14:05:43 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+    . Project description
+        . Topic #1
+        . Topic #2
+*/
+
+#include <fmt/format.h>
+
+import utilities;
+
+int main() {
+    fmt::print("Hello World\n");
+    return 0;
+}
